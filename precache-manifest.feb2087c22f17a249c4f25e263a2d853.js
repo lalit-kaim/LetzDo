0c16480991b5075d222b5ce00153e72ef4e7ee6f
lalit-kaim <lalit-kaim>
2020-04-26 22:29:01 +0530

    Updates

---
+++ precache-manifest.feb2087c22f17a249c4f25e263a2d853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de161dd82e950fa87b6dc7a494a00cac",
+    "url": "/LetzDo/index.html"
+  },
+  {
+    "revision": "ef56e9856cf9d224b50e",
+    "url": "/LetzDo/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "efc80145345efb528015",
+    "url": "/LetzDo/static/css/main.793f6cdf.chunk.css"
+  },
+  {
+    "revision": "ef56e9856cf9d224b50e",
+    "url": "/LetzDo/static/js/2.aa017102.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/LetzDo/static/js/2.aa017102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc80145345efb528015",
+    "url": "/LetzDo/static/js/main.b94596b0.chunk.js"
+  },
+  {
+    "revision": "f6271465a20c0b095ecb",
+    "url": "/LetzDo/static/js/runtime-main.24cb46ab.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/LetzDo/precache-manifest.feb2087c22f17a249c4f25e263a2d853.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LetzDo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
